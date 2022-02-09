98cd91d3e8a7be23094c8e01b1a7219a0532c4c5
LuaCreationist <whatsonmymindreplies@gmail.com>
2022-02-08 21:52:44 -0500

    Updated Player Sprite / Changed Music
    Copyright Ethan Vance Ramsay 2022, for player.png , originally "Pill"
    
    Copyright Jeffery Wellman 2022 for "Pill Guy.wav"

--- libs/audioplayer.lua
+++ libs/audioplayer.lua
@@ -1,7 +1,7 @@
 local audio = {} 
 
 function audio:load_tracks()
-	audio.track = love.audio.newSource("music/CalmNight.mp3","static")
+	audio.track = love.audio.newSource("music/Pill Guy.wav","static")
 	audio.track:setLooping(true)
 end
 function audio:play() 
